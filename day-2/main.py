473e37425f1dc297ef0b074d104f8d0ea48918a0
HLH - Lam The Hao <hlh@elca.vn>
2019-12-03 11:36:51 +0700

    day 2 solutions

--- day-2/main.py
+++ day-2/main.py
@@ -1,22 +1,33 @@
 def get_input():
     with open('2-input.txt', 'r') as file:
-        return file.readline().split(',')
+        return list(map(int,file.readline().split(',')))
 
 def compute(array, pos):
-    if array[pos] == str(99):
+    if int(array[pos]) == 99:
         return 0
     else:
-        array[pos + 3] = int(array[pos + 1]) + int(array[pos + 2])
-        print(array[pos +3])
+        result = 0
+        result_2 = 0
+        if array[pos] == 1:
+            result_2 = array[array[pos + 1]] + array[array[pos + 2]]
+            result = 100*array[array[pos + 1]] + array[array[pos + 2]]
+        else: 
+            result_2 = array[array[pos + 1]] * array[array[pos + 2]]
+            result = 100*array[array[pos + 1]] * array[array[pos + 2]]
+        array[array[pos + 3]] = result_2
+        print(result)
+        if result == 19690720:
+            print('noun: ' + str(array[array[pos + 1]]))
+            print('verb: ' + str(array[array[pos + 2]]))
         return 4
 
-list = get_input()
-
+input_list = get_input()
 pos = 0
-while pos <= list.__len__():
-    result = compute(list, pos)
+while pos <= input_list.__len__():
+    result = compute(input_list, pos)
     if result == 0:
         break
-    else: pos += result
+    else: 
+        pos += result
+        input_list = get_input()
 
-print(list[0])
